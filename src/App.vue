a24d3c91fb24b10fc410a8df56b78ff06681cce9
Miracle <51058450+devMiracle@users.noreply.github.com>
2020-12-23 11:28:55 +0200

    add materialize-css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+    <a class="waves-effect waves-light btn">button</a>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
